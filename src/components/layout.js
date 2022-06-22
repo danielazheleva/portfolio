88a9259c3533f2224e78ec76fabd823b3ba16d3a
danielazheleva <xdaniela.z@gmail.com>
2022-06-22 19:46:48 +0300

    remove examples

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,6 +1,6 @@
 import * as React from 'react'
 import { useStaticQuery, graphql } from 'gatsby'
-import Navigation from '../components/navigation'
+import Navigation from './navigation'
 import '../styles/layout.css'
 
 const Layout = ({ pageTitle, children }) => {
